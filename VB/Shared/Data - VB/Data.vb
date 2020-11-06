9707fb7cf5d47e5558cfff1de10a968dca841f85
DevExpressExampleBot <fakeEmail>
2020-11-06 12:48:43 +0300

    autogenerate VB

---
+++ VB/Shared/Data - VB/Data.vb
@@ -0,0 +1,63 @@
+﻿Imports System
+Imports System.Data.Common
+Imports System.Data.Entity
+Imports System.Data.SQLite
+
+Namespace DXSample.Data
+	Public Class AppointmentEntity
+		Public Property Id() As Integer
+		Public Property AllDay() As Boolean
+		Public Property Subject() As String
+		Public Property Description() As String
+		Public Property Location() As String
+		Public Property Start() As DateTime
+		Public Property [End]() As DateTime
+		Public Property QueryStart() As DateTime
+		Public Property QueryEnd() As DateTime
+		Public Property AppointmentType() As Integer
+		Public Property RecurrenceInfo() As String
+		Public Property ReminderInfo() As String
+		Public Property ResourceId() As Integer?
+		Public Property Label() As Integer
+		Public Property Status() As Integer
+	End Class
+	Public Class ResourceEntity
+		Public Property Id() As Integer
+		Public Property Description() As String
+		Public Property Group() As String
+	End Class
+	Public Class SchedulingContext
+		Inherits DbContext
+
+		Private Const FileName As String = "..\..\..\scheduling.db"
+		Public Sub New()
+			MyBase.New(CreateConnection(), True)
+		End Sub
+		Private Shared Function CreateConnection() As DbConnection
+			Dim connection = DbProviderFactories.GetFactory("System.Data.SQLite.EF6").CreateConnection()
+			connection.ConnectionString = New SQLiteConnectionStringBuilder With {.DataSource = FileName}.ConnectionString
+			Return connection
+		End Function
+		Public Property AppointmentEntities() As DbSet(Of AppointmentEntity)
+		Public Property ResourceEntities() As DbSet(Of ResourceEntity)
+	End Class
+
+	Public Class AppointmentEntityHelper
+		Public Shared Sub CopyProperties(ByVal source As AppointmentEntity, ByVal target As AppointmentEntity)
+			target.AllDay = source.AllDay
+			target.AppointmentType = source.AppointmentType
+			target.Description = source.Description
+			target.End = source.End
+			target.Label = source.Label
+			target.Location = source.Location
+			target.QueryEnd = source.QueryEnd
+			target.QueryStart = source.QueryStart
+			target.RecurrenceInfo = source.RecurrenceInfo
+			target.ReminderInfo = source.ReminderInfo
+			target.ResourceId = source.ResourceId
+			target.Start = source.Start
+			target.Status = source.Status
+			target.Subject = source.Subject
+		End Sub
+	End Class
+End Namespace
---
+++ VB/Shared/Views - VB/SchedulingView.xaml.vb
@@ -0,0 +1,49 @@
+﻿Imports System
+Imports System.Collections.Specialized
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.Native
+Imports DevExpress.Xpf.Scheduling
+
+Namespace DXSample.Views
+	Partial Public Class SchedulingView
+		Inherits UserControl
+
+		Public ReadOnly Property Scheduler() As SchedulerControl
+			Get
+				Return Me.scheduler
+			End Get
+		End Property
+		Public Sub New()
+			InitializeComponent()
+			AddHandler scheduler.ResourceItems.CollectionChanged, AddressOf OnResourceItemsCollectionChanged
+			AddHandler scheduler.AppointmentAdded, Function(d, e) e.Appointments.ForEach(Sub(x) LogChange(x, "added"))
+			AddHandler scheduler.AppointmentEdited, Function(d, e) e.Appointments.ForEach(Sub(x) LogChange(x, "edited"))
+			AddHandler scheduler.AppointmentRemoved, Function(d, e) e.Appointments.ForEach(Sub(x) LogChange(x, "removed"))
+			AddHandler scheduler.AppointmentRestored, Function(d, e) e.Appointments.ForEach(Sub(x) LogChange(x, "restored"))
+			AddHandler scheduler.DataSource.FetchAppointments, Sub(d, e) LogFetch(e.Interval, e.Result.Length)
+			AddHandler refreshData.Click, AddressOf OnRefreshDataClick
+		End Sub
+		Private Sub LogFetch(ByVal interval As DateTimeRange, ByVal count As Integer)
+			Dim intervalStr = interval.ToString("({0:d})-({1:d})", Nothing)
+			Dim format = "{0}: {1} appointments fetched"
+			logTextEdit.Text += String.Format(format, intervalStr, count) & Environment.NewLine
+		End Sub
+		Private Sub LogChange(ByVal appt As AppointmentItem, ByVal action As String)
+			logTextEdit.Text += String.Format("Appointment '{0}' {1}; changes saved to database", appt.Subject, action) & Environment.NewLine
+		End Sub
+		Private Sub OnRefreshDataClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			scheduler.RefreshData(RefreshDataKind.Appointments)
+		End Sub
+		Private Sub ClearLog(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			logTextEdit.Text = String.Empty
+		End Sub
+		Private Sub OnResourceItemsCollectionChanged(ByVal sender As Object, ByVal e As NotifyCollectionChangedEventArgs)
+			For i As Integer = 0 To scheduler.ResourceItems.Count - 1
+				scheduler.ResourceItems(i).Visible = i < 2
+			Next i
+			RemoveHandler scheduler.ResourceItems.CollectionChanged, AddressOf OnResourceItemsCollectionChanged
+		End Sub
+	End Class
+End Namespace
